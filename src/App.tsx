cec2dc15699f131e9a4603d981ff750d67637ca5
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-07-29 14:12:46 +0330

    set data to inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { AccountForm } from "./AccountForm";
 import { AddressForm } from "./AddressForm";
 import "./App.css";
 import { UserForm } from "./UserForm";
 import { useMultistepForm } from "./useMultistepform";
 
+type FormData ={
+  firstName: string,
+  lastName: string,
+  age: string,
+  street: string,
+  city: string,
+  state: string,
+  zip: string,
+  email: string,
+  password: string,
+}
+
+const INITIAL_DATA : FormData = {
+  firstName : "",
+  lastName : "",
+  age : "",
+  street : "",
+  city : "",
+  state : "",
+  zip : "",
+  email : "",
+  password : "",
+}
+
 function App() {
-  const { steps, currentStepIndex, step, isFirstStep, back, next, goTo , isLastStep } =
+  const [data , setData] = useState(INITIAL_DATA);
+  function updateFields (fields:Partial<FormData>){
+   setData(perv => {
+    return {...perv , ...fields}
+   })
+  }
+  const { steps, currentStepIndex, step, isFirstStep, back, next , isLastStep } =
     useMultistepForm([
-      <UserForm/>,<AddressForm/>,<AccountForm/>
+      <UserForm {...data} updateFields={updateFields}/>,
+      <AddressForm {...data} updateFields={updateFields}/>,
+      <AccountForm {...data} updateFields={updateFields}/>
     ]);
 
     function onSubmit (e:FormEvent){
      e.preventDefault();
-     next()
+     if(!isLastStep) return next()
+     alert("Successful Account Creation")
     }
 
   return (
